62d22d09ce3e72eea2f334b6a85c9ee1b23a3ec7
jein-sh <jein.sh85@gmail.com>
2022-07-10 20:45:47 +0200

    добавляет компоненты страниц

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+
+type AppPageProps = {
+  offersCount: number;
+}
+
+function App({offersCount}: AppPageProps): JSX.Element {
+  return (
+    <MainPage offersCount={offersCount} />
+  );
 }
 
 export default App;
